98157fd8b210d09799924a1c7087ef12d7e3f10b
Dario Camonita <danterolle@catania.linux.it>
2023-01-24 11:58:54 +0100

    Added cURL example in main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Example: http://localhost:8080/packages?branch=main&arch=amd64 ~16 mb
+// curl --request GET 'http://localhost:8080/packages?branch=main&arch=amd64'
 func main() {
 	http.HandleFunc("/packages", h.HandleJSONData)
 	http.ListenAndServe(":8080", nil)
