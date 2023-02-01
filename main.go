d79da9afb53b7d49c31f132513c276bf11914338
Dario Camonita <danterolle@catania.linux.it>
2023-02-01 23:51:15 +0100

    Added a very simple feedback from ListenAndServe

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	h "packages-api/handlers"
 )
@@ -9,4 +10,6 @@ import (
 func main() {
 	http.HandleFunc("/packages", h.GetPackage)
 	http.ListenAndServe(":8080", nil)
+	log.Println("Endpoint /packages set, check http://localhost:8080")
+	log.Println("Try curl --request GET 'http://localhost:8080/packages?branch=main&arch=amd64&package=nginx'")
 }
